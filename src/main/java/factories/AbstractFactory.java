ace2d9a3451b236f1608dc1f3c7141c7272adae2
Dmytro Shtrikker <shtrikker28@gmail.com>
2023-10-11 22:47:25 +0300

    structured files

--- src/main/java/factories/AbstractFactory.java
+++ src/main/java/factories/AbstractFactory.java
@@ -3,10 +3,6 @@ package factories;
 import circles.Circle;
 import triangles.Triangle;
 
-/**
- * Hello world!
- *
- */
 public interface AbstractFactory {
     public Circle createCircle();
     public Triangle createTriangle();
